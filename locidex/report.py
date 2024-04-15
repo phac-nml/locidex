5b8b71bb449c04e8e422d7058d16ab188f819d6a
James <james.robertson@phac-aspc.gc.ca>
2024-04-15 09:52:39 -0400

    added int stop filter

--- locidex/report.py
+++ locidex/report.py
@@ -73,9 +73,10 @@ class seq_reporter:
             if ambig_count > self.max_ambig_count or stop_count > self.max_int_stop_count:
                 failed_seqids.add(seq_id)
             if self.mode == 'conservative':
+                count_internal_stop = self.query_seq_data[seq_id]['count_internal_stop']
                 start_codon = self.query_seq_data[seq_id]["start_codon"]
                 stop_codon = self.query_seq_data[seq_id]["stop_codon"]
-                if start_codon not in START_CODONS or stop_codon not in STOP_CODONS:
+                if start_codon not in START_CODONS or stop_codon not in STOP_CODONS or count_internal_stop > 0:
                     failed_seqids.add(seq_id)
                 
         self.failed_seqids =  failed_seqids
