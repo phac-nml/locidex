b1045064d06aa3e2e15f4d5916521101824cac8e
Matthew Wells <mattwells9@shaw.ca>
2024-05-13 16:12:05 -0500

    added more merge tests

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -34,5 +34,6 @@ def test_extract_profiles():
     records = merge.read_file_list(MERGE_SUCCESSFULLY, perform_validation=True)
     extracted_profiles = merge.extract_profiles(records)
     assert len(extracted_profiles) == 2
-    key1, key2 = extracted_profiles.keys()
-    assert extracted_profiles[key1] == extracted_profiles[key2]
+    value1, value2 = extracted_profiles.values()
+    assert value1 == value2
+    assert value1 == {'aroC': '9048803cd72dee3c868cd2dc5dc5650d', 'dnaN': '2772ad8b8e0f7b50f1396c31fbe53f2d', 'hemD': '620f99723c4e190abe096b11ca34b944', 'hisD': '38027ac1ac34817584a176c7e575e97e', 'purE': '9855cbf4009439498bf84cacefce4d8f', 'sucA': '9289fc07cc8e93cfe0716e6f613cefdb', 'thrA': '9e1aa76bb42279ed7ec8fc30f984b65d'}
