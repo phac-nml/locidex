5c3bf55d874d4a5d287dc362f8eb831adea9c385
James <james.robertson@phac-aspc.gc.ca>
2025-03-26 14:58:06 -0400

    added new override parameter

--- locidex/search.py
+++ locidex/search.py
@@ -55,6 +55,8 @@ def add_args(parser=None):
                         default=80.0)
     parser.add_argument('--max_target_seqs', type=int, required=False, help='Maximum number of hit seqs per query',
                         default=10)
+    parser.add_argument('--override', required=False, help='Overwrite individual loci thresholds for filtering and use the global parameters',
+                        action='store_true')
     parser.add_argument('--n_threads','-t', type=int, required=False,
                         help='CPU Threads to use', default=1)
     parser.add_argument('--format', type=str, required=False,
@@ -126,6 +128,7 @@ def run_search(config):
     sample_name = config['name']
     perform_annotation = config['annotate']
     max_target_seqs = config['max_target_seqs']
+    override = config['override']
 
     if max_count := config.get('max_ambig_count'):
         max_ambig_count = max_count
